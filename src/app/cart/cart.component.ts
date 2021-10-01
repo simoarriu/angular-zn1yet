94fa7417167c2b761cdf9bf1efd09d3d863e55ee
simoarriu <simoarriu@gmail.com>
2021-10-01 14:15:31 +0200

    Cart done

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
+  //Define the items property to store the products in the cart.
+  items = this.cartService.getItems();
 
-  constructor() { }
+  //Inject the CartService so that the CartComponent can use it by adding it to the constructor().
+  constructor(private cartService: CartService) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
